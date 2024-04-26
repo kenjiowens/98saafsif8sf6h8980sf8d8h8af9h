b09e6279224790122022e33f7413c3ec480b4fb9
fishychris1 <161517014+fishychris1@users.noreply.github.com>
2024-04-25 17:34:50 -0700

    idek

---
+++ 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/assets/jsfiles/pass2.js
@@ -0,0 +1,9 @@
+var password;
+var pass1="fishy";
+password=prompt('Enter Password To View Page','');
+if (password==pass1) {
+    alert('Correct password, Click OK to enter');
+} else {
+    alert('Wrong, try again');
+    location.href = "https://project-blizzard.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair"
+};
