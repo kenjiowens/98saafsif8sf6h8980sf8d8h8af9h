a4fd07f547be4a50faa7211abc3185d52af569b9
Fish <115324554+Bwaf@users.noreply.github.com>
2024-04-29 09:28:57 -0700

    ahhh

--- 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/JS/LoginInfo.js
+++ 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/JS/LoginInfo.js
@@ -1,5 +1,6 @@
 function guestUserJoke(){
     location.href = "https://project-blizzard.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html";  
+    //https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/login.html
   }
   
   function checkUser(){
