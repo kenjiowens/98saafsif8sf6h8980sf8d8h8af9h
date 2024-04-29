98e61f4c02b12cabc50c4116036835c63b7ab072
Fish <115324554+Bwaf@users.noreply.github.com>
2024-04-29 10:03:27 -0700

    fixed all the linking probs with the js file for logininfo

--- 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/JS/LoginInfo.js
+++ 12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/JS/LoginInfo.js
@@ -18,9 +18,9 @@ function guestUserJoke(){
       //Christopher Novales
       if(document.getElementById('username').value == 'supernova'){
         if(document.getElementById('passcode').value == 'itzmV'){
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/a9sd80f8080dsfhcritthefurspecial10sf/burgerwopperwithUturnSignsForLifeonchair/";
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/a9sd80f8080dsfhcritthefurspecial10sf/burgerwopperwithUturnSignsForLifeonchair/";
           } else {
-            location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
+            location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
           //alert('Wrong Passcode or Username');
           return false;
           }
@@ -28,9 +28,9 @@ function guestUserJoke(){
       //Andrew Seitz
       if(document.getElementById('username').value == 'LitleGecki10'){
           if(document.getElementById('passcode').value == 'goplaywiifit'){
-              location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/1k516v12cds159sg9geckovb9xcvb9/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/";
+              location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/1k516v12cds159sg9geckovb9xcvb9/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/";
           } else {
-            location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
+            location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
           //alert{'Wrong Passcode or Username'};
           return false;
       }
@@ -38,44 +38,44 @@ function guestUserJoke(){
       //Kenji Owens
       if(document.getElementById('username').value == 'kenjiowen3'){
         if(document.getElementById('passcode').value == 'bloons'){
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair";
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/aV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair";
         } else {
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
           return false;
         }
       }
       //Jaxon schieder
       if(document.getElementById('username').value == 'fryking'){
         if(document.getElementById('passcode').value == 'cheese'){
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/asdfkl1j2ej12v90frykingspecial/burgerwopperwithUturnSignsForLifeonchair/";
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/asdfkl1j2ej12v90frykingspecial/burgerwopperwithUturnSignsForLifeonchair/";
         } else {
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
           return false;
         }
       }
       //justin Azzi
       if(document.getElementById('username').value == 'bob'){
         if(document.getElementById('passcode').value == 'water'){
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/1c091x0JustinAzziziSpecial19x1xxxx120/burgerwopperwithUturnSignsForLifeonchair";  
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/1c091x0JustinAzziziSpecial19x1xxxx120/burgerwopperwithUturnSignsForLifeonchair";  
         } else {
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
           return false;
         }
       }
       //Avery Rose
       if(document.getElementById('username').value == 'avy'){
         if(document.getElementById('passcode').value == 'ross'){
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/averoseleraV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html";  
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/averoseleraV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html";  
         } else {
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
           return false;
         }
       }
       if(document.getElementById('username').value == 'guest'){
         if(document.getElementById('passcode').value == 'guest'){
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html";  
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html";  
         } else {
-          location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
+          location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html"
           return false;
         }
       }
@@ -88,7 +88,7 @@ function guestUserJoke(){
                   if(document.getElementById('username').value != 'bob'){
                     if(document.getElementById('username').value != 'guest'){
                       if(document.getElementById('username').value != 'avy'){
-                        location.href = "https://cnofppzafzyv.github.io/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html";
+                        location.href = "https://cnofppzafzyv.github.io/98saafsif8sf6h8980sf8d8h8af9h/12vl1239Blizzard123b3v1v9cas0g/15ad9asd9g7zcx987cbc98b98/guesswhat/guestaV12519x12eJv01jeAsd91jaS09190Xxx/burgerwopperwithUturnSignsForLifeonchair/index.html";
          }
         }
         }
